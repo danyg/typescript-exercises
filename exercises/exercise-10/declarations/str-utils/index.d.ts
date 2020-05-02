ba9c6bbf0927def029676019ed8f34af66110a4f
danyg <dalgo86@gmail.com>
2020-05-02 09:26:50 +0200

    Eleventh Excercise Done

--- exercises/exercise-10/declarations/str-utils/index.d.ts
+++ exercises/exercise-10/declarations/str-utils/index.d.ts
@@ -1,4 +1,12 @@
+type TransformStringFunction = (value: string) => string;
+
 declare module 'str-utils' {
-    // export const ...
-    // export function ...
+	// export const ...
+	// export function ...
+
+	export const strReverse: TransformStringFunction;
+	export const strToLower: TransformStringFunction;
+	export const strToUpper: TransformStringFunction;
+	export const strRandomize: TransformStringFunction;
+	export const strInvertCase: TransformStringFunction;
 }
