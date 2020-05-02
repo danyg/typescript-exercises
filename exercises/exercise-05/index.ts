52a5acc5b58e3a03eb456dd7c32c3a21767b4027
danyg <dalgo86@gmail.com>
2020-05-02 07:32:30 +0200

    Sixth Excercise Done

--- exercises/exercise-05/index.ts
+++ exercises/exercise-05/index.ts
@@ -34,45 +34,56 @@ Run:
 */
 
 interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
+	type: 'user';
+	name: string;
+	age: number;
+	occupation: string;
 }
 
 interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
+	type: 'admin';
+	name: string;
+	age: number;
+	role: string;
 }
 
 type Person = User | Admin;
 
+type NoTypeProperty<T> = Pick<T, Exclude<keyof T, 'type'>>
+type FilterCriteria<T> = { [P in keyof NoTypeProperty<T>]+?: NoTypeProperty<T>[P] };
+type FilterCriteriaUser = FilterCriteria<User>;
+type FilterCriteriaAdmin = FilterCriteria<Admin>;
+
 const persons: Person[] = [
-    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
-    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+	{ type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+	{ type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+	{ type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+	{ type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+	{ type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+	{ type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
 ];
 
 function logPerson(person: Person) {
-    console.log(
-        ` - ${chalk.green(person.name)}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
-    );
+	console.log(
+		` - ${chalk.green(person.name)}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
+	);
 }
 
-function filterPersons(persons: Person[], personType: string, criteria: unknown): unknown[] {
-    return persons
-        .filter((person) => person.type === personType)
-        .filter((person) => {
-            let criteriaKeys = Object.keys(criteria) as (keyof Person)[];
-            return criteriaKeys.every((fieldName) => {
-                return person[fieldName] === criteria[fieldName];
-            });
-        });
+function getObjectKeys<T>(object: T): (keyof T)[] {
+	return Object.keys(object) as (keyof T)[];
+}
+
+function filterPersons(persons: Person[], personType: "user", criteria: FilterCriteriaUser): User[];
+function filterPersons(persons: Person[], personType: "admin", criteria: FilterCriteriaAdmin): Admin[];
+function filterPersons(persons: Person[], personType: string, criteria: FilterCriteria<User | Admin>): (User | Admin)[] {
+	return persons
+		.filter((person) => person.type === personType)
+		.filter((person) => {
+			const criteriaKeys = getObjectKeys(criteria);
+			return criteriaKeys.every((fieldName) => {
+				return person[fieldName] === criteria[fieldName];
+			});
+		});
 }
 
 let usersOfAge23: User[] = filterPersons(persons, 'user', { age: 23 });
